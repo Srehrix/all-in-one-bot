67f00fa51b06c2901641fe06fd9b86debe42bf29
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2023-06-24 13:07:48 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -157,3 +157,6 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
     else:
         await quer_y.message.edit(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
     await quer_y.answer()
+@Client.on_message(filters.command('set_template'))
+async def save_template(client, message):
+    sts = await message.reply("Checking template")
